e6b6d2458fb8beed94890752851cadadd21f69a3
pongv <pongvarid@gmail.com>
2021-11-08 13:25:47 +0700

    change bg color

--- src/pages/root/Login.vue
+++ src/pages/root/Login.vue
@@ -2,11 +2,11 @@
 <q-page>
     <section class="min-h-screen flex items-stretch text-white ">
         <div class="lg:w-1/2 w-full flex items-center justify-center text-center md:px-16 px-0 z-0" style="background-color: #161616;">
-            <div class="absolute lg:hidden z-10 inset-0 bg-gray-500 bg-no-repeat bg-cover items-center" style="background-image: url(https://images.unsplash.com/photo-1577495508048-b635879837f1?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80);">
+            <div class="absolute lg:hidden z-10 inset-0 bg-gray-500 bg-no-repeat bg-cover items-center" style="background-image: url(https://sv1.picz.in.th/images/2021/11/08/uXFjPb.png);">
                 <div class="absolute bg-black opacity-60 inset-0 z-0"></div>
-            </div>
+            </div> 
             <div class="w-full py-6 z-20">
-                <h1 class="my-6 text-4xl">
+                <h1 class="my-6 text-4xl font-semibold ">
                     Heart Exercise
                 </h1>
                 <div class="py-6 space-x-2">
