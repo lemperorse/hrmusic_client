b93a6db98ab75857738b5de53071326cf4fdce98
pongvarid <pongvarid@gmail.com>
2022-10-10 02:16:10 +0700

    fix graph

--- src/components/Programs/DurationGraph.vue
+++ src/components/Programs/DurationGraph.vue
@@ -1,7 +1,6 @@
 <template>
 <q-card  class="m-2 p-2 w-full"  style="overflow-x: scroll"> 
-    <div v-if="response" style="width:1080px">
-
+    <div v-if="response" :style="`width:${(length > 10)?1600:1080}px`">  
         <VueApexCharts  v-if="response" type="line" height="350" :options="chartOptions" :series="series"></VueApexCharts>
 
     </div>
@@ -11,7 +10,7 @@
 <script>
 import VueApexCharts from 'vue-apexcharts'
 import { initial } from 'lodash'
-
+import moment from'moment'
 export default {
     components: {
         VueApexCharts
@@ -64,6 +63,8 @@ export default {
                     },
                 }
             },
+            times:[],
+            length:0,
             response: false,
         })
     },
@@ -80,24 +81,46 @@ export default {
             let reps = []
             let time = []
             let allTime = 0
-
-            for (var i = 0; i < this.durations.length; i++) {
-
-                for (var j = 0; j < this.durations[i].reps; j++) {
+            let dataTime = moment('1996-05-16 00:00:00', 'YYYY-MM-DD HH:mm:ss')
+            for (var i = 0; i < this.durations.length; i++) { 
+                if(i == 0){
+                        console.log(i)
+                        time.push('00:00:00')
+                    }
+                for (var j = 0; j < this.durations[i].reps; j++) {  
                     data.push(this.durations[i].place)
                     data.push(this.durations[i].rest)
                     let inTime = this.getMin(this.durations[i].duration, this.durations[i].rest_duration)
+                  
+                    // let duration = Number(moment(`1996-05-16 ${this.durations[i].duration}`).format(`mm.ss`))   //this.getTmeToInt(this.durations[i].duration)
+                    // let rest_duration = Number(moment(`1996-05-16 ${this.durations[i].rest_duration}`).format(`mm.ss`)) // this.getTmeToInt(this.durations[i].rest_duration)
 
-                    let duration = this.getTmeToInt(this.durations[i].duration)
-                    let rest_duration = this.getTmeToInt(this.durations[i].rest_duration)
+                    let duration_min = Number(moment(`1996-05-16 ${this.durations[i].duration}`).format(`mm`))   
+                    let duration_sec = Number(moment(`1996-05-16 ${this.durations[i].duration}`).format(`ss`))    
+                    let rest_duration_min = Number(moment(`1996-05-16 ${this.durations[i].rest_duration}`).format(`mm`))  
+                    let rest_duration_sec = Number(moment(`1996-05-16 ${this.durations[i].rest_duration}`).format(`ss`))  
+                    
+                    dataTime.add(duration_min, 'minutes').add(duration_sec, 'seconds') 
+                    time.push((dataTime).format(`HH:mm:ss`))
+                    this.times.push(dataTime.format(`HH:mm:ss`))
+                    dataTime.add(rest_duration_min, 'minutes').add(rest_duration_sec, 'seconds')
+                    time.push((dataTime).format(`HH:mm:ss`))
+                    this.times.push(dataTime.format(`HH:mm:ss`)) 
 
-                    allTime += duration
-                    time.push(`${allTime} min`)
-                    allTime += rest_duration
-                    time.push(`${allTime} min`)
+                    // allTime += duration
+                    // time.push(`${allTime} min`)
+                    // allTime += rest_duration
+                    // time.push(`${allTime} min`)
+                    // var now = 
+
+                    // dataTime.add(this.durations[i].duration)
+                    // console.log(Number(now))
+                    //  time.push(this.durations[i].duration)
+                    //  time.push(this.durations[i].rest_duration)
                 }
 
             }
+            this.length = data.length
             this.series.push({
                 name: "duration",
                 data: data
@@ -105,6 +128,7 @@ export default {
 
             console.log(data)
             this.chartOptions.xaxis.categories = time
+            //this.times = time
             this.response = true
 
         },
